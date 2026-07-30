4731a01a150d319788815f428b356550a30a3bc2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-30 14:47:24 +0500

    update AuthLayout component

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -1,14 +1,4 @@
 
-// export const AuthLayout = () => {
-//     return (
-//         <div className="min-h-screen flex items-center justify-center">
-//             <div className="w-full">
-//                 <Outlet />
-//             </div>
-//         </div>
-//     );
-// };
-
 import { Outlet } from "react-router-dom";
 export const AuthLayout = () => {
     return (
