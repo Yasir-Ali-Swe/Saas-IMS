7f99fa5c8dca5007c8fdb778d6d7e3a23b251ac2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-02 00:01:19 +0500

    update server index

--- server/index.js
+++ server/index.js
@@ -24,14 +24,17 @@ import { scheduleAnomalyJob } from "./jobs/anomaly.cron.js";
 import { scheduleInsightsJob } from "./jobs/insights.cron.js";
 import { scheduleReorderSuggestionJob } from "./jobs/reorderSuggestion.cron.js";
 import morgan from "morgan";
+import { createIndexes } from "./config/indexes.js";
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 app.use("/api/v1/billing", billingRoutes);
@@ -60,6 +63,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
+await createIndexes();
 
 // ============ BACKGROUND JOBS ============
 scheduleForecastJob();
